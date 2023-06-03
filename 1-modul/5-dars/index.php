cf57f9049bd8fc27272927b32bf650371f30eee6
e-az1z <ergashevazizjon189@gmail.com>
2023-06-03 22:24:26 +0500

    darslar

---
+++ 1-modul/5-dars/index.php
@@ -0,0 +1,62 @@
+<?php
+
+	// $x = 26;
+	// $y = 5;
+
+	// echo gettype($x);
+
+	// $z = $x % $y; // 26 / 5 = oldidagi sonni hisoblanmaydi 5.1
+	// $z =3 ** 4; // (3*3) * (3*3) = 81
+
+	// $z = $x; //$z = $x = 26
+	// $z += $y; //$z = $y = 5
+	// $z -= $y; //$z = 26 - 5
+	// $z *= $y; //$z = 26 * 5
+	// $z /= $y++; //$z = 26 / 5 = 5.1++ = 5.2
+	// $z .= $y; // String qo'shish $z = 26 . 5
+	// echo $z--.
+	// $z; // 265264
+
+	// $x = 10;
+	// $x++; E'lon qilingandan keyin 1 ga oshiradi;
+	// ++$x; 1 ga oshirib e'lon qilinadi;
+	// $x--; E'lon qilingandan keyin 1 ga kamaytiradi;
+	// --$x; 1 ga kamaytirib e'lon qilinadi;
+	// $x = 10;
+	// echo $x--; //9
+
+	// == - qiymatni tekshiradi;
+	// === - qiymatni va turini tekshiradi;
+	// != - teng emas qiymat;
+	// !== - teng emas qiymat va turi;
+	// > - katta bo'lsa;
+	// < - kichik bo'lsa;
+	// >= - katta yoki teng bo'lsa;
+	// <= - kichik yoki teng bo'lsa;
+	// <=> - ;
+	// <> - Teng Emas
+	// $x = 5;// integer
+	// $y = '6';// string
+	// echo ($x <> $y);
+
+	// ? - if()
+	// : - else()
+	// . - String
+	// .= -
+	// $x = 10;
+	// $y = 15;
+	// echo ($x == $y) ? "Rost" : "Xatolik";
+	// $x = 5;
+	// $y = &$x;
+	// $x = 56;
+	// echo $y;
+
+	// and - &&
+	// or -
+	// xor
+
+	// $x = 16;
+	// $y = 18;
+
+	// echo ($x >= $y  $x <= $y) ? "18 yoshga to'lgan" : "hali kichkina";
+?>
